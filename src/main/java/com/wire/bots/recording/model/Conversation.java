0fb68a4969b241d49438e92ff04353c2fd5a0112
dkovacevic <dejankov@gmail.com>
2019-08-02 12:59:46 +0200

    Removed extra code

--- src/main/java/com/wire/bots/recording/model/Conversation.java
+++
@@ -1,9 +0,0 @@
-package com.wire.bots.recording.model;
-
-
-import java.util.LinkedList;
-
-public class Conversation {
-    public LinkedList<Day> days = new LinkedList<>();
-    public String title;
-}
--- src/main/java/com/wire/bots/recording/model/Message.java
+++
@@ -1,8 +0,0 @@
-package com.wire.bots.recording.model;
-
-public class Message {
-    public String text;
-    public String image;
-    public String time;
-    public String likes;
-}
