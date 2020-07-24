0e682750745a7858969801f4e816f24115f9d5a8
dkovacevic <dejankov@gmail.com>
2020-07-24 12:37:33 +0200

    Added proto handler

--- src/main/java/com/wire/bots/recording/commands/BackupIosCommand.java
+++ src/main/java/com/wire/bots/recording/commands/BackupIosCommand.java
@@ -1,12 +1,17 @@
 package com.wire.bots.recording.commands;
 
+import com.waz.model.Messages;
+import com.wire.bots.recording.utils.Collector;
+import com.wire.bots.recording.utils.InstantCache;
+import com.wire.bots.sdk.exceptions.HttpException;
+import com.wire.bots.sdk.models.*;
 import io.dropwizard.setup.Bootstrap;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 import pw.forst.wire.backups.ios.GenericMessageDto;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import static pw.forst.wire.backups.ios.GenericMessageConverterKt.obtainProtobufsForDatabase;
 
@@ -25,14 +30,68 @@ public class BackupIosCommand extends BackupCommandBase {
                 .type(String.class)
                 .required(true)
                 .help("Extracted iOS database");
+
+        subparser.addArgument("-e", "--email")
+                .dest("email")
+                .type(String.class)
+                .required(true)
+                .help("Email address");
+
+        subparser.addArgument("-p", "--password")
+                .dest("password")
+                .type(String.class)
+                .required(true)
+                .help("Password");
     }
 
     @Override
-    public void run(Bootstrap<?> bootstrap, Namespace namespace) {
+    public void run(Bootstrap<?> bootstrap, Namespace namespace) throws HttpException {
         System.out.printf("Backup to PDF converter version: %s\n\n", VERSION);
 
         final String in = namespace.getString("in");
+        final String email = namespace.getString("email");
+        final String password = namespace.getString("password");
+
+        InstantCache cache = new InstantCache(email, password, getClient(bootstrap));
+
+        UUID from = UUID.randomUUID();
+        String clientId = "";
+        UUID convId = UUID.randomUUID();
+        String time = "2020-07-12T21:12:02.123Z";
+
+        Collector collector = new Collector(cache);
+
         List<GenericMessageDto> messages = obtainProtobufsForDatabase(in);
-        messages.forEach(m -> System.out.println(Arrays.toString(m.getProtobuf())));
+        for (GenericMessageDto msg : messages) {
+            try {
+                final Messages.GenericMessage genericMessage = Messages.GenericMessage.parseFrom(msg.getProtobuf());
+                final MessageBase messageBase = GenericMessageConverter.convert(from, clientId, convId, time, genericMessage);
+
+                if (messageBase instanceof TextMessage) {
+                    collector.add((TextMessage) messageBase);
+                }
+                if (messageBase instanceof ImageMessage) {
+                    collector.add((ImageMessage) messageBase);
+                }
+                if (messageBase instanceof VideoMessage) {
+                    collector.add((VideoMessage) messageBase);
+                }
+                if (messageBase instanceof AttachmentMessage) {
+                    collector.add((AttachmentMessage) messageBase);
+                }
+                if (messageBase instanceof ReactionMessage) {
+                    collector.add((ReactionMessage) messageBase);
+                }
+                if (messageBase instanceof LinkPreviewMessage) {
+                    collector.addLink((LinkPreviewMessage) messageBase);
+                }
+                if (messageBase instanceof EditedTextMessage) {
+                    collector.addEdit((EditedTextMessage) messageBase);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
