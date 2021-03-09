ace5c039ec156928b05059c8eb1224a82d170ed8
Lukas Forst <lukas@forst.pw>
2021-03-09 16:35:49 +0100

    exit with non-zero code when no client is passed

--- src/main/java/com/wire/backups/exports/commands/ExportToolCommand.java
+++ src/main/java/com/wire/backups/exports/commands/ExportToolCommand.java
@@ -6,11 +6,13 @@ import java.util.concurrent.Callable;
 
 @Command(
         name = "base",
-        description = "Export Wire client backups."
+        description = "Export Wire client backups.",
+        mixinStandardHelpOptions = true
 )
 public class ExportToolCommand implements Callable<Integer> {
     @Override
     public Integer call() {
-        return 0;
+        System.out.println("Pass client name - web, android or ios.");
+        return 1;
     }
 }
